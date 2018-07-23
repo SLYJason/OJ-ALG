4260d2d93ed5018a79f35419bd5c4575ea7e8993
Luyi Song <songly91.6.1@gmail.com>
2018-07-23 17:20:58 -0400

    LeetCode-771

---
+++ src/main/java/LeetCode771.java
@@ -0,0 +1,18 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class LeetCode771 {
+    public int numJewelsInStones(String J, String S) {
+        int total = 0;
+        Set set = new HashSet<Character>();
+        for(char j:J.toCharArray()) {
+            set.add(j);
+        }
+        for(char s:S.toCharArray()) {
+            if(set.contains(s)) {
+                total++;
+            }
+        }
+        return total;
+    }
+}
