76a9615b2bfe811e783f2e42353bf9d441100fb7
Luyi Song <songly91.6.1@gmail.com>
2019-07-09 22:46:17 -0400

    LeetCode-937

---
+++ src/main/java/LeetCode/LeetCode937.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class LeetCode937 {
+    public String[] reorderLogFiles(String[] logs) {
+        Arrays.sort(logs, (log1, log2) -> {
+            String[] split1 = log1.split(" ", 2);
+            String[] split2 = log2.split(" ", 2);
+            boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+            boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+
+            if (!isDigit1 && !isDigit2) {
+                int comLetterLog = split1[1].compareTo(split2[1]);
+                if (comLetterLog != 0) return comLetterLog;
+                return split1[0].compareTo(split2[0]);
+            }
+            return isDigit1 ? (isDigit2 ? 0 : 1) : -1;
+        });
+        return logs;
+    }
+}
