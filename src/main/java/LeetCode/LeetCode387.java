64beda3f35335977f1d07890c4d2396c6e03a454
Luyi Song <songly91.6.1@gmail.com>
2019-07-11 13:42:50 -0400

    LeetCode-387

---
+++ src/main/java/LeetCode/LeetCode387.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+public class LeetCode387 {
+    public int firstUniqChar(String s) {
+        int[] array = new int[26];
+        // Building an array that stores each character appearance times
+        for(Character ch:s.toCharArray()) {
+            array[ch-'a']++;
+        }
+        // Find the index
+        for (int i=0; i<s.length(); i++) {
+            if (array[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
