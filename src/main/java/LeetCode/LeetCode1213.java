2f172824c52ca64f2c1e993657de43085d5e6082
Luyi Song <songly91.6.1@gmail.com>
2019-10-10 13:17:54 -0400

    LeetCode-1213

---
+++ src/main/java/LeetCode/LeetCode1213.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LeetCode1213 {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> list = new LinkedList<Integer>();
+        int p1 = 0, p2 = 0, p3 = 0;
+        while (p1 < arr1.length && p2 < arr2.length && p3 < arr3.length) {
+            int min = Math.min(arr1[p1], Math.min(arr2[p2], arr3[p3]));
+            if (arr1[p1] == arr2[p2] && arr2[p2] == arr3[p3]) {
+                list.add(arr1[p1]);
+            }
+            if (arr1[p1] == min) p1++;
+            if (arr2[p2] == min) p2++;
+            if (arr3[p3] == min) p3++;
+        }
+        return list;
+    }
+}
