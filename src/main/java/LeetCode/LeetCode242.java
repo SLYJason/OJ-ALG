8a73f93978ba1fe2be83a50a18185bd72b1c6900
Luyi Song <songly91.6.1@gmail.com>
2020-02-18 13:08:56 -0500

    LeetCode-242

---
+++ src/main/java/LeetCode/LeetCode242.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+public class LeetCode242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] array = new int[26];
+        for(char ch : s.toCharArray()) array[ch - 'a']++;
+        for(char ch : t.toCharArray()) {
+            if (--array[ch - 'a'] < 0) return false;
+        }
+        return true;
+    }
+}
