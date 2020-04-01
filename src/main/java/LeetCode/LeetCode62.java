beee2f0129067c404c1b4d3551d789eb5a8c311c
Luyi Song <songly91.6.1@gmail.com>
2020-04-01 14:56:45 -0400

    LeetCode-62

---
+++ src/main/java/LeetCode/LeetCode62.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class LeetCode62 {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int[] arr : dp) {
+            Arrays.fill(arr, 1);
+        }
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
