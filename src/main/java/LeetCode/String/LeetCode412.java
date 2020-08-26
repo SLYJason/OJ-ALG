05c6eb0b4322aea834d638c300356a19b431269f
Luyi Song <songly91.6.1@gmail.com>
2020-08-26 15:49:48 -0400

    LeetCode-412

---
+++ src/main/java/LeetCode/String/LeetCode412.java
@@ -0,0 +1,31 @@
+package LeetCode.String;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Solution 1: do not use %.
+ */
+public class LeetCode412 {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<String>(n);
+        for(int i=1,fizz=0,buzz=0;i<=n ;i++){
+            fizz++;
+            buzz++;
+            if(fizz==3 && buzz==5){
+                res.add("FizzBuzz");
+                fizz=0;
+                buzz=0;
+            }else if(fizz==3){
+                res.add("Fizz");
+                fizz=0;
+            }else if(buzz==5){
+                res.add("Buzz");
+                buzz=0;
+            }else{
+                res.add(String.valueOf(i));
+            }
+        }
+        return res;
+    }
+}
