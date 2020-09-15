ab560ef183f5b012e46b8096d2b94f2af15b2055
Luyi Song <songly91.6.1@gmail.com>
2020-09-15 16:06:46 -0400

    LeetCode-210

--- src/main/java/LeetCode/Graph/LeetCode210.java
+++ src/main/java/LeetCode/Graph/LeetCode210.java
@@ -6,29 +6,38 @@ import java.util.ArrayList;
 /**
  * Solution: Topological Sort.
  */
-public class LeetCode207 {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+public class LeetCode210 {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> graph = new ArrayList();
         for(int i=0; i<numCourses; i++) graph.add(new ArrayList());
         for(int[] prerequisite : prerequisites) {
             graph.get(prerequisite[1]).add(prerequisite[0]);
         }
+
         // 0: unvisited, 1: visiting, 2: visited.
         int[] states = new int[numCourses];
+        List<Integer> res = new ArrayList();
         for(int i=0; i<numCourses; i++) {
-            if(hasCyclic(i, states, graph)) return false;
+            if(hasCyclic(i, states, graph, res)) return new int[0];
         }
-        return true;
+        // building the result.
+        int[] ans = new int[res.size()];
+        for(int i=0; i<ans.length; i++) {
+            ans[i] = res.get(res.size()-i-1);
+        }
+        return ans;
     }
 
-    private boolean hasCyclic(int curr, int[] states, List<List<Integer>> graph) {
+    private boolean hasCyclic(int curr, int[] states, List<List<Integer>> graph, List<Integer> res) {
         if(states[curr] == 1) return true;
         if(states[curr] == 2) return false;
         states[curr] = 1;
+
         for(int neighbor : graph.get(curr)) {
-            if(hasCyclic(neighbor, states, graph)) return true;
+            if(hasCyclic(neighbor, states, graph, res)) return true;
         }
         states[curr] = 2;
+        res.add(curr);
         return false;
     }
 }
--- src/main/java/LeetCode/Graph/README.md
+++ src/main/java/LeetCode/Graph/README.md
@@ -1,2 +1,6 @@
 # Course Schedule (Topological Sort)
 Directed Acyclic Graph (DAG) has a valid topological ordering.
+
+207 (Medium): Course Schedule.
+
+210 (Medium): Course Schedule II.
