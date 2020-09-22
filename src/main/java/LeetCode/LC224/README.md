a102bd2905e0ba5f1c7bfe8e3be1c5964a37bd6d
Luyi Song <songly91.6.1@gmail.com>
2020-09-21 20:06:10 -0400

    LeetCode-227

--- src/main/java/LeetCode/LC224/README.md
+++ src/main/java/LeetCode/LC224/README.md
@@ -1,1 +1,6 @@
 # 224: Basic Calculator
+Related: 
+
+[224-Basic Calculator](../LC224).
+
+[227-Basic Calculator II](../LC227).
---
+++ src/main/java/LeetCode/LC227/Solution.java
@@ -0,0 +1,32 @@
+package LeetCode.LC227;
+
+public class Solution {
+    public int calculate(String s) {
+        if(s.length() == 0) return 0;
+        int res = 0;
+        int sign = 1;
+
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            int num = 0;
+            if(Character.isDigit(ch)) {
+                num = s.charAt(i) - '0';
+                while(i+1 < s.length() && Character.isDigit(s.charAt(i+1))) {
+                    num = num * 10 + (s.charAt(i+1) - '0');
+                    i++;
+                }
+
+                if(i+1 < s.length() && s.charAt(i+1) == '+') {
+
+                } else if(i+1 < s.length() && s.charAt(i+1) == '-') {
+
+                } else if(i+1 < s.length() && s.charAt(i+1) == '*') {
+
+                } else if(i+1 < s.length() && s.charAt(i+1) == '/') {
+
+                }
+            }
+        }
+        return res;
+    }
+}
