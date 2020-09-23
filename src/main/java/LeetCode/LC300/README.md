a7c4908baa8eed97e2ca0e505058e18d886123c3
Luyi Song <songly91.6.1@gmail.com>
2020-09-23 11:06:53 -0400

    LeetCode-300

---
+++ src/main/java/LeetCode/LC300/README.md
@@ -0,0 +1,5 @@
+# 300: Longest Increasing Subsequence
+
+![chart](LC300.png)
+
+Reference: [Huahua](http://zxi.mytechroad.com/blog/dynamic-programming/leetcode-300-longest-increasing-subsequence/).
---
+++ src/main/java/LeetCode/LC300/Solution.java
@@ -0,0 +1,27 @@
+package LeetCode.LC300;
+
+import java.util.Arrays;
+
+/**
+ * Solution: DP.
+ * Time Complexity: O(n^2).
+ * Space Complexity: O(n).
+ */
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int max = 0;
+        int[] f = new int[nums.length];
+        Arrays.fill(f, 1);
+        for(int i=1; i<nums.length; i++) {
+            for(int j=0; j<i; j++) {
+                if(nums[i] > nums[j]) {
+                    f[i] = Math.max(f[i], f[j]+1);
+                }
+            }
+        }
+        for(int i=0; i<f.length; i++) {
+            max = Math.max(max, f[i]);
+        }
+        return max;
+    }
+}
