64840c6de457db149627e2c4f9d4f9826836adef
Luyi Song <songly91.6.1@gmail.com>
2020-09-24 13:37:38 -0400

    LeetCode-703

---
+++ src/main/java/LeetCode/LC703/Solution.java
@@ -0,0 +1,28 @@
+package LeetCode.LC703;
+
+import java.util.PriorityQueue;
+
+/**
+ * Solution: using PriorityQueue, min-heap.
+ */
+public class Solution {
+    class KthLargest {
+        int k;
+        int[] nums;
+        PriorityQueue<Integer> pq;
+        public KthLargest(int k, int[] nums) {
+            this.k = k;
+            this.nums = nums;
+            this.pq = new PriorityQueue<Integer>(k);
+            for(int n : nums) add(n);
+        }
+
+        public int add(int val) {
+            pq.offer(val);
+            if(pq.size() > k) {
+                pq.poll();
+            }
+            return pq.peek();
+        }
+    }
+}
