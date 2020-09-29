05f19d40e84104ddcc2772f16355906ccd519178
Luyi Song <songly91.6.1@gmail.com>
2020-09-29 19:44:18 -0400

    LeetCode-133

---
+++ src/main/java/LeetCode/LC133/Solution_1.java
@@ -0,0 +1,28 @@
+package LeetCode.LC133;
+
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * Solution 1: DFS.
+ */
+public class Solution_1 {
+    public Node cloneGraph(Node node) {
+        return dfs(node, new HashMap());
+    }
+
+    private Node dfs(Node node, Map<Node, Node> map) {
+        if(node == null) return null;
+        if(map.containsKey(node)) return map.get(node);
+        Node copy = new Node(node.val);
+        map.put(node, copy);
+        for(Node neighbor : node.neighbors) {
+            copy.neighbors.add(dfs(neighbor, map));
+        }
+        return copy;
+    }
+}
+
+
+
+
