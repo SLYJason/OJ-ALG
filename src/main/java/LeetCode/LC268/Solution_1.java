5df34327b4ff8fab8c3092b3df247fa35c86e800
Luyi Song <songly91.6.1@gmail.com>
2020-09-29 20:11:17 -0400

    LeetCode-268

---
+++ src/main/java/LeetCode/LC268/Solution_1.java
@@ -0,0 +1,13 @@
+package LeetCode.LC268;
+
+/**
+ * Solution 1: brute-force.
+ */
+public class Solution_1 {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for(int i=0; i<=nums.length; i++) sum += i;
+        for(int n : nums) sum -= n;
+        return sum;
+    }
+}
