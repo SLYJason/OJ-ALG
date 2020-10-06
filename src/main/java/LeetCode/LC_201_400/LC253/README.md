92ecd048a277bae42143e9c04bc1c94949f824b0
Luyi Song <songly91.6.1@gmail.com>
2020-10-06 14:07:48 -0400

    LeetCode-253

---
+++ src/main/java/LeetCode/LC_201_400/LC253/README.md
@@ -0,0 +1,9 @@
+# 253: Meeting Rooms II
+
+Related:
+
+56: Merge Intervals.
+
+252: Meeting Rooms.
+
+253: Meeting Rooms II.
---
+++ src/main/java/LeetCode/LC_201_400/LC253/Solution.java
@@ -0,0 +1,22 @@
+package LeetCode.LC_201_400.LC253;
+
+import java.util.PriorityQueue;
+import java.util.Arrays;
+
+public class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if(intervals.length == 0 || intervals.length == 1) return intervals.length;
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // min-heap
+        minHeap.offer(intervals[0][1]);
+        for(int i=1; i<intervals.length; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            if(start >= minHeap.peek()) {
+                minHeap.poll();
+            }
+            minHeap.offer(end);
+        }
+        return minHeap.size();
+    }
+}
