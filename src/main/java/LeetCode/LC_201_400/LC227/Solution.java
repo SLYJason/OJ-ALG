74dabef8854a68a888c99833969f4caacd938248
Luyi Song <songly91.6.1@gmail.com>
2020-10-06 20:02:59 -0400

    LeetCode-227

--- src/main/java/LeetCode/LC_201_400/LC227/Solution.java
+++ src/main/java/LeetCode/LC_201_400/LC227/Solution.java
@@ -1,32 +1,40 @@
 package LeetCode.LC_201_400.LC227;
 
+import java.util.Stack;
+
 public class Solution {
     public int calculate(String s) {
         if(s.length() == 0) return 0;
-        int res = 0;
-        int sign = 1;
-
+        int num = 0;
+        char sign = '+';
+        s += "+";
+        Stack<Integer> stack = new Stack();
         for(int i=0; i<s.length(); i++) {
             char ch = s.charAt(i);
-            int num = 0;
+            if(ch == ' ') continue; // skip blank.
             if(Character.isDigit(ch)) {
-                num = s.charAt(i) - '0';
+                num = ch - '0';
                 while(i+1 < s.length() && Character.isDigit(s.charAt(i+1))) {
-                    num = num * 10 + (s.charAt(i+1) - '0');
+                    num = num * 10 + s.charAt(i+1) - '0';
                     i++;
                 }
-
-                if(i+1 < s.length() && s.charAt(i+1) == '+') {
-
-                } else if(i+1 < s.length() && s.charAt(i+1) == '-') {
-
-                } else if(i+1 < s.length() && s.charAt(i+1) == '*') {
-
-                } else if(i+1 < s.length() && s.charAt(i+1) == '/') {
-
-                }
+                continue; // find a number.
             }
+            // ch is a sign.
+            if(sign == '+') {
+                stack.push(num);
+            } else if(sign == '-') {
+                stack.push(-num);
+            } else if(sign == '*') {
+                stack.push(stack.pop()*num);
+            } else if(sign == '/') {
+                stack.push(stack.pop()/num);
+            }
+            sign = ch;
         }
-        return res;
+
+        int sum = 0;
+        while(!stack.isEmpty()) sum += stack.pop();
+        return sum;
     }
 }
