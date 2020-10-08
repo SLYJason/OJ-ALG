36d64d5dc09bba41d696ba3bc1f21e9e2f2e76a0
Luyi Song <songly91.6.1@gmail.com>
2020-10-07 20:37:57 -0400

    LeetCode-125: redo

---
+++ src/main/java/LeetCode/LC_1_200/LC125/LeetCode125.java
@@ -0,0 +1,18 @@
+package LeetCode.LC_1_200.LC125;
+
+public class LeetCode125 {
+    public boolean isPalindrome(String s) {
+        if(s.length() == 0 || s.length() == 1) return true;
+        int i=0, j=s.length()-1;
+        while(i<j) {
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+            char ch1 = s.charAt(i);
+            char ch2 = s.charAt(j);
+            if(Character.toLowerCase(ch1) != Character.toLowerCase(ch2)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/LeetCode/LC_1_200/LC125/README.md
@@ -0,0 +1,7 @@
+# 125: Valid Palindrome
+
+Related:
+
+125: Valid Palindrome.
+
+680: Valid Palindrome II.
