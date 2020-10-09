9883f9f29ef5e0fd90ac952fb7dfef712644f974
Luyi Song <songly91.6.1@gmail.com>
2020-10-08 22:23:46 -0400

    LeetCode-349

---
+++ src/main/java/LeetCode/LC_201_400/LC349/Solution_1.java
@@ -0,0 +1,19 @@
+package LeetCode.LC_201_400.LC349;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * Solution 1: two sets.
+ */
+public class Solution_1 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet();
+        Set<Integer> set2 = new HashSet();
+        for(int num : nums1) set1.add(num);
+        for(int num : nums2) {
+            if(set1.contains(num)) set2.add(num);
+        }
+        return set2.stream().mapToInt(n -> n).toArray();
+    }
+}
---
+++ src/main/java/LeetCode/LC_201_400/LC349/Solution_2.java
@@ -0,0 +1,29 @@
+package LeetCode.LC_201_400.LC349;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Arrays;
+
+/**
+ * Solution 2: sorting + two-pointers.
+ */
+public class Solution_2 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i=0, j=0;
+        while(i<nums1.length && j<nums2.length) {
+            if(nums1[i] == nums2[j]) {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            } else if(nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return set.stream().mapToInt(n -> n).toArray();
+    }
+}
