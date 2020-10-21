0ad42c5e27e6fd167dd733329b36d0a5f5e37057
Luyi Song <songly91.6.1@gmail.com>
2020-10-21 13:13:27 -0400

    LeetCode-128: sorting brute force another implementation

--- src/main/java/LeetCode/LC_1_200/LC128/Solution_1.java
+++ src/main/java/LeetCode/LC_1_200/LC128/Solution_1.java
@@ -1,13 +1,14 @@
 package LeetCode.LC_1_200.LC128;
 
 import java.util.Arrays;
-/**
- * Solution 1: sorting + sliding window.
- * Refer: LC674.
- * Time Complexity: O(NlogN).
- * Space Complexity: O(1).
- */
+
 public class Solution_1 {
+    /**
+     * Solution 1: sorting + sliding window.
+     * Refer: LC674.
+     * Time Complexity: O(NlogN).
+     * Space Complexity: O(1).
+     */
     public int longestConsecutive(int[] nums) {
         int l = nums.length;
         if(l <= 1) return l;
@@ -23,5 +24,25 @@ public class Solution_1 {
             }
         }
         return res;
+    }
+
+    /**
+     * Another implementation, using sorting not using sliding window.
+     */
+    public int longestConsecutive2(int[] nums) {
+        if(nums.length <= 1) return nums.length;
+        Arrays.sort(nums);
+        int max = 1, max_so_far = 1;
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i] == nums[i-1]) {
+                max = Math.max(max, max_so_far);
+            } else if(nums[i] - nums[i-1] == 1) {
+                max_so_far++;
+                max = Math.max(max, max_so_far);
+            } else{
+                max_so_far = 1;
+            }
+        }
+        return max;
     }
 }
