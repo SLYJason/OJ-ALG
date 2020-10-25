328894df58a04d72e59275e7f1deeb5c541c0d06
Luyi Song <songly91.6.1@gmail.com>
2020-10-24 20:18:19 -0400

    LeetCode-152: DP array solution

--- src/main/java/LeetCode/LC_1_200/LC152/Solution_2.java
+++ src/main/java/LeetCode/LC_1_200/LC152/Solution_2.java
@@ -1,24 +1,29 @@
 package LeetCode.LC_1_200.LC152;
 
 /**
- * Solution 2: DP.
- * For a given nums[i], current max product can be:
- * 1> nums[i] * previous_max_product, e.g. current is 5 and previous_max_product is 10.
- * 2> nums[i] * previous_min_product, e.g. current is -5 and previous_min_product is -10.
- * 3> nums[i] itself, e.g. curr has preceding 0: [0, 5].
+ * Solution 2: DP array, refer LC53 DP Solution_1.
  */
 public class Solution_2 {
     public int maxProduct(int[] nums) {
         if(nums.length == 0) return 0;
-        int max_so_far = nums[0];
-        int min_so_far = nums[0];
-        int result = nums[0];
+        Tuple[] dp = new Tuple[nums.length];
+        dp[0] = new Tuple(nums[0], nums[0]);
+        int max = nums[0];
         for(int i=1; i<nums.length; i++) {
-            int temp = max_so_far; // important, since max_so_far will be updated in next line.
-            max_so_far = Math.max(Math.max(max_so_far*nums[i], min_so_far*nums[i]), nums[i]);
-            min_so_far = Math.min(Math.min(min_so_far*nums[i], temp*nums[i]), nums[i]);
-            result = Math.max(result, max_so_far);
+            int max_so_far = Math.max(nums[i], Math.max(dp[i-1].max_so_far * nums[i], dp[i-1].min_so_far * nums[i]));
+            int min_so_far = Math.min(nums[i], Math.min(dp[i-1].max_so_far * nums[i], dp[i-1].min_so_far * nums[i]));
+            dp[i] = new Tuple(max_so_far, min_so_far);
+            max = Math.max(max, Math.max(max_so_far, min_so_far));
         }
-        return result;
+        return max;
+    }
+
+    class Tuple {
+        public int max_so_far; // max product ending in nums[i] inclusively.
+        public int min_so_far; // min product ending in nums[i] inclusively.
+        public Tuple(int max, int min) {
+            max_so_far = max;
+            min_so_far = min;
+        }
     }
 }
---
+++ src/main/java/LeetCode/LC_1_200/LC152/Solution_3.java
@@ -0,0 +1,24 @@
+package LeetCode.LC_1_200.LC152;
+
+/**
+ * Solution 3: DP.
+ * For a given nums[i], current max product can be:
+ * 1> nums[i] * previous_max_product, e.g. current is 5 and previous_max_product is 10.
+ * 2> nums[i] * previous_min_product, e.g. current is -5 and previous_min_product is -10.
+ * 3> nums[i] itself, e.g. curr has preceding 0: [0, 5].
+ */
+public class Solution_3 {
+    public int maxProduct(int[] nums) {
+        if(nums.length == 0) return 0;
+        int max_so_far = nums[0];
+        int min_so_far = nums[0];
+        int result = nums[0];
+        for(int i=1; i<nums.length; i++) {
+            int temp = max_so_far; // important, since max_so_far will be updated in next line.
+            max_so_far = Math.max(Math.max(max_so_far*nums[i], min_so_far*nums[i]), nums[i]);
+            min_so_far = Math.min(Math.min(min_so_far*nums[i], temp*nums[i]), nums[i]);
+            result = Math.max(result, max_so_far);
+        }
+        return result;
+    }
+}
