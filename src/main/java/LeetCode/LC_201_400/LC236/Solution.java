c2efbec5f3af67f0aed3377ea7ada395b0fff7ba
Luyi Song <songly91.6.1@gmail.com>
2020-12-12 17:38:37 -0500

    LeetCode-236

--- src/main/java/LeetCode/LC_201_400/LC236/Solution.java
+++ src/main/java/LeetCode/LC_201_400/LC236/Solution.java
@@ -2,12 +2,20 @@ package LeetCode.LC_201_400.LC236;
 
 import Shared.TreeNode;
 
+/**
+ * Solution: recursion.
+ */
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q) return root;
+        if(root == null) return null;
+        if(root == p || root == q) return root;
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if(left != null && right != null) return root;
-        return left == null ? right : left;
+        if(left != null && right != null) return root; // LCA found.
+        if(left == null && right == null) return null; // Both p and q not found.
+        if(left == null) {
+            return right; // one of p or q found.
+        }
+        return left; // one of p or q found.
     }
 }
