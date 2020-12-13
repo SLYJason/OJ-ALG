58c0c989d24a782082375e46141df1c9227d3c3b
Luyi Song <songly91.6.1@gmail.com>
2020-12-13 17:00:42 -0500

    LeetCode-235

---
+++ src/main/java/LeetCode/LC_201_400/LC235/Solution_1.java
@@ -0,0 +1,17 @@
+package LeetCode.LC_201_400.LC235;
+
+import Shared.TreeNode;
+
+/**
+ * Solution: recursive.
+ */
+public class Solution_1 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(p.val <= root.val && q.val >= root.val || q.val <= root.val && p.val >= root.val) return root;
+        if(p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+    }
+}
---
+++ src/main/java/LeetCode/LC_201_400/LC235/Solution_2.java
@@ -0,0 +1,22 @@
+package LeetCode.LC_201_400.LC235;
+
+import Shared.TreeNode;
+
+/**
+ * Solution: iterative.
+ */
+public class Solution_2 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode parent = root;
+        while(parent != null) {
+            if(p.val < parent.val && q.val < parent.val) {
+                parent = parent.left;
+            } else if(p.val > parent.val && q.val > parent.val) {
+                parent = parent.right;
+            } else {
+                return parent;
+            }
+        }
+        return null;
+    }
+}
