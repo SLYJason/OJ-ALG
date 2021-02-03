5f97d5fbdfb4597d1e3491acbb9d41d6c3f73463
Luyi Song <songly91.6.1@gmail.com>
2021-02-03 11:32:11 -0500

    LeetCode-141

---
+++ src/main/java/LeetCode/LC_1_200/LC141/README.md
@@ -0,0 +1,11 @@
+# 141: Linked List Cycle
+
+### Solution: Floyd's Tortoise and Hare Algorithm
+**Time Complexity:** `O(N)`.
+
+**Space Complexity:** `O(1)`.
+
+### Related
+141: Linked List Cycle.
+
+142: Linked List Cycle II.
--- src/main/java/LeetCode/LC_1_200/LC141/Solution.java
+++ src/main/java/LeetCode/LC_1_200/LC141/Solution.java
@@ -1,8 +1,11 @@
-package LeetCode.LinkedList;
+package LeetCode.LC_1_200.LC141;
 
 import Shared.ListNode;
 
-public class LeetCode141 {
+/**
+ * Solution: Floyd's Tortoise and Hare algorithm.
+ */
+public class Solution {
     public boolean hasCycle(ListNode head) {
         if(head == null) return false;
         ListNode slow = head;
