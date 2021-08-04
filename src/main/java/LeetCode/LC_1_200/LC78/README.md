569dfaeda31ce8f780edd4b7a32b8ad50a782420
Luyi Song <jasongluyi@gmail.com>
2021-08-04 00:16:48 -0400

    LeetCode-78: redo

--- src/main/java/LeetCode/LC_1_200/LC78/README.md
+++ src/main/java/LeetCode/LC_1_200/LC78/README.md
@@ -1,1 +1,11 @@
 # 78: Subsets
+
+### Solution: Backtrack
+**Time Complexity:** `O(N * 2^N)`, calculate subsets takes `O(2^N)` time since each element could be absent or present, copy them into output list takes `O(N)` time.
+
+**Space Complexity:** `O(N)`.
+
+### Related
+78: Subsets.
+
+90: Subsets II.
--- src/main/java/LeetCode/LC_1_200/LC78/Solution.java
+++ src/main/java/LeetCode/LC_1_200/LC78/Solution.java
@@ -5,17 +5,18 @@ import java.util.ArrayList;
 
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList();
-        dfs(0, nums, new ArrayList(), res);
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(0, nums, new ArrayList<>(), res);
         return res;
     }
 
-    private void dfs(int start, int[] nums, List<Integer> list, List<List<Integer>> res) {
-        res.add(new ArrayList(list));
-        for(int i=start; i<nums.length; i++) {
+    private void backtrack(int start, int[] nums, List<Integer> list, List<List<Integer>> res) {
+        res.add(new ArrayList<>(list));
+        if(start == nums.length) return;
+        for(int i = start; i < nums.length; i++) {
             list.add(nums[i]);
-            dfs(i+1, nums, list, res);
-            list.remove(list.size()-1);
+            backtrack(i + 1, nums, list, res);
+            list.remove(list.size() - 1);
         }
     }
 }
