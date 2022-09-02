e71fd46bb1c1eadc71212672e49b95632a26bd67
Luyi Song <jasongluyi@gmail.com>
2022-09-02 00:13:43 -0400

    LeetCode-2239

---
+++ src/main/java/LeetCode/LC_2201_2400/README.md
@@ -0,0 +1,13 @@
+# 2239: Find Closest Number to Zero
+
+### Solution
+Key observation: At max situation, there are two numbers closest to 0, one negative and one positive so the positive one is the answer.
+
+**Time Complexity:** `O(N)`.
+
+**Space Complexity:** `O(1)`.
+
+### Related
+658: Find K Closest Elements.
+
+2239: Find Closest Number to Zero.
---
+++ src/main/java/LeetCode/LC_2201_2400/Solution.java
@@ -0,0 +1,13 @@
+package LeetCode.LC_2201_2400;
+
+public class Solution {
+    public int findClosestNumber(int[] nums) {
+        int result = Integer.MAX_VALUE;
+        for(int n : nums) {
+            if (Math.abs(n) < Math.abs(result) || n == Math.abs(result)) {
+                result = n;
+            }
+        }
+        return result;
+    }
+}
