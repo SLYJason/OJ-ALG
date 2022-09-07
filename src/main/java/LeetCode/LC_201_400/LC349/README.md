0fba8002d803b5cce15dd9d3e44ca78f9521d063
Luyi Song <jasongluyi@gmail.com>
2022-09-07 00:02:14 -0400

    LeetCode-349: format

--- src/main/java/LeetCode/LC_201_400/LC349/README.md
+++ src/main/java/LeetCode/LC_201_400/LC349/README.md
@@ -1,7 +1,16 @@
 # 349: Intersection of Two Arrays
 
-Related:
+### Solution 1: Two Sets
+**Time Complexity**: `O(M + N)`. `M` and `N` are the size of array `nums1` and `nums2`.
 
+**Space Complexity**: `O(M + N)`.
+
+### Solution 2: Sorting + Two Pointers
+**Time Complexity**: `O(M * logM + N * logN)`.
+
+**Space Complexity**: `O(min(M, N))`.
+
+### Related
 349: Intersection of Two Arrays.
 
 350: Intersection of Two Arrays II.
--- src/main/java/LeetCode/LC_201_400/LC349/Solution_2.java
+++ src/main/java/LeetCode/LC_201_400/LC349/Solution_2.java
@@ -9,16 +9,16 @@ import java.util.Arrays;
  */
 public class Solution_2 {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set = new HashSet();
+        Set<Integer> set = new HashSet<>();
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        int i=0, j=0;
-        while(i<nums1.length && j<nums2.length) {
-            if(nums1[i] == nums2[j]) {
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
                 set.add(nums1[i]);
                 i++;
                 j++;
-            } else if(nums1[i] < nums2[j]) {
+            } else if (nums1[i] < nums2[j]) {
                 i++;
             } else {
                 j++;
