fe2dbd84e0d92330e273c3cfe71d6016c87011be
Luyi Song <jasongluyi@gmail.com>
2022-09-09 14:54:46 -0400

    LeetCode-57: redo

--- src/main/java/LeetCode/LC_1_200/LC57/README.md
+++ src/main/java/LeetCode/LC_1_200/LC57/README.md
@@ -1,11 +1,23 @@
 # 57: Insert Interval
 
-Related:
+### Solution
+1. Add the intervals to the output list before the `newInterval` start position.
+2. Keep merging the interval as long as current interval start position is less than the `newInterval` end position.
+3. Merge the rest of intervals.
 
+**Time Complexity**: `O(N)`.
+
+**Space Complexity**: `O(1)`.
+
+### Related
 56: Merge Intervals.
 
-57. Insert Interval.
+57: Insert Interval.
 
 252: Meeting Rooms.
 
 253: Meeting Rooms II.
+
+616: Add Bold Tag in StringAdd Bold Tag in String.
+
+758: Bold Words in String.
--- src/main/java/LeetCode/LC_1_200/LC57/Solution.java
+++ src/main/java/LeetCode/LC_1_200/LC57/Solution.java
@@ -1,28 +1,32 @@
 package LeetCode.LC_1_200.LC57;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * Solution: greedy.
+ */
 public class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        if(intervals.length == 0) return new int[][]{newInterval};
-        List<int[]> merged = new ArrayList();
-        int i=0;
+        if (intervals.length == 0) return new int[][]{newInterval};
+        List<int[]> list = new LinkedList<>();
+        int i = 0;
         // step 1: add all intervals before newInterval, no overlap.
-        while(i<intervals.length && intervals[i][1] < newInterval[0]) {
-            merged.add(intervals[i++]);
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            list.add(intervals[i++]);
         }
         // step 2: merge overlap.
-        while(i<intervals.length && newInterval[1] >= intervals[i][0]) {
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
             newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
             newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             i++;
         }
-        merged.add(newInterval);
-        // step 3: add all intervals after the newInterval, no overlap.
-        while(i<intervals.length && newInterval[1] < intervals[i][0]) {
-            merged.add(intervals[i++]);
+        list.add(newInterval);
+        // step 3: add the rest.
+        while (i < intervals.length) {
+            list.add(intervals[i++]);
         }
-        return merged.toArray(new int[merged.size()][2]);
+        return list.toArray(new int[list.size()][2]);
     }
 }
