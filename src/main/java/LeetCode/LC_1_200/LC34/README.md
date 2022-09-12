c1540518f3411e48d14a13828ef1748b88c16647
Luyi Song <jasongluyi@gmail.com>
2022-09-11 23:55:02 -0400

    LeetCode-34: redo

--- src/main/java/LeetCode/LC_1_200/LC34/README.md
+++ src/main/java/LeetCode/LC_1_200/LC34/README.md
@@ -6,3 +6,8 @@ The idea is using 2 times binary search to find `first` index and `last` index r
 **Time Complexity:** `O(logN)`.
 
 **Space Complexity:** `O(1)`.
+
+### Follow Up
+Q: Find the number of occurrence of k in a sorted array.
+
+A: Just find the first and last index of k in the sorted array, then the answer is `first - last + 1`.
--- src/main/java/LeetCode/LC_1_200/LC34/Solution.java
+++ src/main/java/LeetCode/LC_1_200/LC34/Solution.java
@@ -5,20 +5,21 @@ package LeetCode.LC_1_200.LC34;
  */
 public class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums.length == 0) return new int[]{-1, -1};
+        if (nums.length == 0) return new int[]{-1, -1};
         int first = findFirst(nums, target);
-        if(first == -1) return new int[]{-1, -1};
+        if (first == -1) return new int[]{-1, -1};
         int last = findLast(nums, target);
         return new int[]{first, last};
+
     }
 
-    public int findFirst(int[] nums, int target) {
-        int left = 0, right = nums.length-1;
-        while(left < right) {
+    private int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
             int mid = left + (right - left) / 2; // get lower bound.
-            if(nums[mid] == target) {
+            if (nums[mid] == target) {
                 right = mid;
-            } else if(nums[mid] < target) {
+            } else if (nums[mid] < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
@@ -27,18 +28,18 @@ public class Solution {
         return nums[left] == target ? left : -1; // check if we find target index.
     }
 
-    public int findLast(int[] nums, int target) {
-        int left = 0, right = nums.length-1;
-        while(left < right) {
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
             int mid = left + (right - left + 1) / 2; // get upper bound.
-            if(nums[mid] == target) {
+            if (nums[mid] == target) {
                 left = mid;
-            } else if(nums[mid] < target) {
+            } else if (nums[mid] < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        return left;
+        return nums[left] == target ? left : -1; // check if we find target index.
     }
 }
