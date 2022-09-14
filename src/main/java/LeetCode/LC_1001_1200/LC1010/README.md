01e0e36cdf6f305918d475e58109bccb96af9209
Luyi Song <jasongluyi@gmail.com>
2022-09-14 18:09:53 -0400

    LeetCode-1010: redo

--- src/main/java/LeetCode/LC_1001_1200/LC1010/README.md
+++ src/main/java/LeetCode/LC_1001_1200/LC1010/README.md
@@ -1,1 +1,8 @@
 # 1010: Pairs of Songs With Total Durations Divisible by 60
+
+### Solution: Two Sum
+The idea is need to get the each `time[i] % 60` first, and the another matching will be `60 - time[i] % 60`. Build a map to record already processed songs and keeping add the result.
+
+**Time Complexity:** `O(N)`.
+
+**Space Complexity:** `O(1)`. Constant space since the size of the map will never above 60.
--- src/main/java/LeetCode/LC_1001_1200/LC1010/Solution.java
+++ src/main/java/LeetCode/LC_1001_1200/LC1010/Solution.java
@@ -4,19 +4,19 @@ import java.util.Map;
 import java.util.HashMap;
 
 /**
- * Solution: similar to Two-Sum.
+ * Solution: two sum.
  */
 public class Solution {
     public int numPairsDivisibleBy60(int[] time) {
         int res = 0;
-        Map<Integer, Integer> map = new HashMap();
-        for(int i=0; i<time.length; i++) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < time.length; i++) {
             int t = time[i] % 60;
             int another = t == 0 ? 0 : 60 - t;
-            if(map.containsKey(another)){
+            if (map.containsKey(another)) {
                 res += map.get(another);
             }
-            map.put(t, map.getOrDefault(t, 0)+1);
+            map.put(t, map.getOrDefault(t, 0) + 1);
         }
         return res;
     }
