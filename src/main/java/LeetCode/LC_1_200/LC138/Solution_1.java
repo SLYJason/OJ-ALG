63ef22282b131a6671a7714d51e958e20f9a9e14
Luyi Song <jasongluyi@gmail.com>
2022-09-15 16:55:09 -0400

    LeetCode-138: format

--- src/main/java/LeetCode/LC_1_200/LC138/Solution_1.java
+++ src/main/java/LeetCode/LC_1_200/LC138/Solution_1.java
@@ -9,20 +9,23 @@ import java.util.HashMap;
 public class Solution_1 {
     public Node copyRandomList(Node head) {
         if(head == null) return null;
+
         Node dummy = new Node(-1);
         Node p1 = head;
         Node p2 = dummy;
         Map<Node, Node> map = new HashMap<>(); // K: original node, V: copied node.
-        // copy each node.
-        while(p1 != null) {
+
+        // step 1: copy each node.
+        while (p1 != null) {
             Node copy = new Node(p1.val);
             p2.next = copy;
             p2 = copy;
             map.put(p1, p2);
             p1 = p1.next;
         }
-        // link random node.
-        for(Map.Entry<Node, Node> entry : map.entrySet()) {
+
+        // step 2: link random node.
+        for (Map.Entry<Node, Node> entry : map.entrySet()) {
             Node ori = entry.getKey();
             Node copy = entry.getValue();
             copy.random = map.get(ori.random);
--- src/main/java/LeetCode/LC_1_200/LC138/Solution_2.java
+++ src/main/java/LeetCode/LC_1_200/LC138/Solution_2.java
@@ -5,28 +5,31 @@ package LeetCode.LC_1_200.LC138;
  */
 public class Solution_2 {
     public Node copyRandomList(Node head) {
-        if(head == null) return null;
+        if (head == null) return null;
+
         Node p1 = head;
-        // copy each node.
-        while(p1 != null) {
+        // step 1: copy each node.
+        while (p1 != null) {
             Node copy = new Node(p1.val);
             Node next = p1.next;
             p1.next = copy;
             copy.next = next;
             p1 = next;
         }
-        // link random node.
+
+        // step 2: link random node.
         p1 = head;
-        while(p1 != null) {
+        while (p1 != null) {
             Node copy = p1.next;
             copy.random = p1.random == null ? null : p1.random.next;
             p1 = copy.next;
         }
-        // link next node, recover the original list.
+
+        // step 3: link next node, recover the original list.
         p1 = head;
         Node p2 = head.next;
         Node copy = head.next; // Important! Since we are modifying the reference.
-        while(p2 != null) {
+        while (p2 != null) {
             Node next = p2.next;
             p1.next = next;
             p2.next = next == null ? null : next.next;
