75bf43cac4913e79d4a25cb089c086c94c9e0363
Luyi Song <jasongluyi@gmail.com>
2022-09-18 16:26:49 -0400

    LeetCode-21 & 23: format

--- src/main/java/LeetCode/LC_1_200/LC21/Solution_1.java
+++ src/main/java/LeetCode/LC_1_200/LC21/Solution_1.java
@@ -7,13 +7,14 @@ import Shared.ListNode;
  */
 public class Solution_1 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null && l2 == null) return null;
-        if(l1 == null) return l2;
-        if(l2 == null) return l1;
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
         ListNode dummy = new ListNode();
         ListNode prev = dummy;
-        while(l1 != null && l2 != null) {
-            if(l1.val <= l2.val) {
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
                 prev.next = l1;
                 l1 = l1.next;
             } else {
--- src/main/java/LeetCode/LC_1_200/LC23/Solution_1.java
+++ src/main/java/LeetCode/LC_1_200/LC23/Solution_1.java
@@ -7,22 +7,22 @@ import Shared.ListNode;
  */
 public class Solution_1 {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0) return null;
-        if(lists.length == 1) return lists[0];
+        if (lists.length == 0) return null;
+        if (lists.length == 1) return lists[0];
         ListNode merged = lists[0];
-        for(int i=1; i<lists.length; i++) {
+        for (int i = 1; i < lists.length; i++) {
             merged = mergeTwoLists(merged, lists[i]);
         }
         return merged;
     }
 
     private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null && l2 == null) return null;
-        if(l1 == null) return l2;
-        if(l2 == null) return l1;
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
         ListNode dummy = new ListNode();
         ListNode prev = dummy;
-        while(l1 != null && l2 != null) {
+        while (l1 != null && l2 != null) {
             if(l1.val <= l2.val) {
                 prev.next = l1;
                 l1 = l1.next;
