fea6ec60c3c33fd417fea22dadc6d67bbbdd962f
Luyi Song <jasongluyi@gmail.com>
2022-09-21 15:42:26 -0400

    LeetCode-151 & 344: format

--- src/main/java/LeetCode/LC_1_200/LC151/Solution.java
+++ src/main/java/LeetCode/LC_1_200/LC151/Solution.java
@@ -5,13 +5,13 @@ package LeetCode.LC_1_200.LC151;
  */
 public class Solution {
     public String reverseWords(String s) {
-        StringBuilder reversed = new StringBuilder();
-        for(int start=s.length()-1; start>-1; start--) {
-            if (s.charAt(start) == ' ') continue;
-            int end = start;
-            while(start > -1 && s.charAt(start) != ' ') start--;
-            reversed.append(s.substring(start+1, end+1)).append(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = s.length()-1; i > -1; i--) {
+            if (s.charAt(i) == ' ') continue;
+            int j = i;
+            while (i > -1 && s.charAt(i) != ' ') i--;
+            sb.append(s, i + 1, j + 1).append(" ");
         }
-        return reversed.toString().trim();
+        return sb.toString().trim();
     }
 }
--- src/main/java/LeetCode/LC_201_400/LC344/Solution.java
+++ src/main/java/LeetCode/LC_201_400/LC344/Solution.java
@@ -5,7 +5,7 @@ package LeetCode.LC_201_400.LC344;
  */
 public class Solution {
     public void reverseString(char[] s) {
-        for(int i=0, j=s.length-1; i<j; i++, j--) {
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
             char temp = s[i];
             s[i] = s[j];
             s[j] = temp;
