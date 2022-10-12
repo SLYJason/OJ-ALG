d7951d305d0902b050ed0daed01d8a286ed11cf1
Luyi Song <jasongluyi@gmail.com>
2022-10-12 13:00:07 -0400

    LeetCode-588: format

--- src/main/java/LeetCode/LC_401_600/LC588/Solution.java
+++ src/main/java/LeetCode/LC_401_600/LC588/Solution.java
@@ -1,4 +1,4 @@
-package LeetCode.Design;
+package LeetCode.LC_401_600.LC588;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.TreeMap;
 import java.util.Collections;
 
-public class LeetCode588 {
+public class Solution {
     class FileSystem {
         Directory root;
         public FileSystem() {
@@ -18,16 +18,16 @@ public class LeetCode588 {
             Directory r = root;
             List<String> files = new ArrayList<>();
             String[] dirs = path.split("/");
-            if(!path.equals("/")) {
-                for(int i=1; i<dirs.length-1; i++) {
+            if (!path.equals("/")) {
+                for (int i = 1; i < dirs.length - 1; i++) {
                     r = r.dirs.get(dirs[i]);
                 }
                 // it is file path, just return the file name.
-                if(r.files.containsKey(dirs[dirs.length-1])) {
-                    files.add(dirs[dirs.length-1]);
+                if (r.files.containsKey(dirs[dirs.length - 1])) {
+                    files.add(dirs[dirs.length - 1]);
                     return files;
                 } else {
-                    r = r.dirs.get(dirs[dirs.length-1]);
+                    r = r.dirs.get(dirs[dirs.length - 1]);
                 }
             }
             files.addAll(new ArrayList<>(r.dirs.keySet()));
@@ -39,8 +39,8 @@ public class LeetCode588 {
         public void mkdir(String path) {
             Directory r = root;
             String[] dirs = path.split("/");
-            for(int i=1; i<dirs.length; i++) {
-                if(!r.dirs.containsKey(dirs[i])) {
+            for (int i = 1; i < dirs.length; i++) {
+                if (!r.dirs.containsKey(dirs[i])) {
                     r.dirs.put(dirs[i], new Directory());
                 }
                 r = r.dirs.get(dirs[i]);
@@ -50,19 +50,19 @@ public class LeetCode588 {
         public void addContentToFile(String filePath, String content) {
             Directory r = root;
             String[] dirs = filePath.split("/");
-            for(int i=1; i<dirs.length-1; i++) {
+            for (int i = 1; i < dirs.length - 1; i++) {
                 r = r.dirs.get(dirs[i]);
             }
-            r.files.put(dirs[dirs.length-1], r.files.getOrDefault(dirs[dirs.length-1], "") + content);
+            r.files.put(dirs[dirs.length - 1], r.files.getOrDefault(dirs[dirs.length - 1], "") + content);
         }
 
         public String readContentFromFile(String filePath) {
             Directory r = root;
             String[] dirs = filePath.split("/");
-            for(int i=1; i<dirs.length-1; i++) {
+            for (int i = 1; i < dirs.length - 1; i++) {
                 r = r.dirs.get(dirs[i]);
             }
-            return r.files.get(dirs[dirs.length-1]);
+            return r.files.get(dirs[dirs.length - 1]);
         }
     }
 
@@ -70,8 +70,8 @@ public class LeetCode588 {
         public Map<String, Directory> dirs;
         public Map<String, String> files;
         public Directory() {
-            this.dirs = new HashMap();
-            this.files = new TreeMap();
+            this.dirs = new HashMap<>();
+            this.files = new TreeMap<>();
         }
     }
 }
