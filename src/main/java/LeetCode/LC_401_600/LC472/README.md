95bd14482140c68322fefc32fa9a735a72da4020
Luyi Song <jasongluyi@gmail.com>
2022-10-11 23:06:09 -0400

    LeetCode-472: format

--- src/main/java/LeetCode/LC_401_600/LC472/README.md
+++ src/main/java/LeetCode/LC_401_600/LC472/README.md
@@ -1,7 +1,13 @@
 # 472: Concatenated Words
 
-Related:
+### Solution: DP
+Similar to LC139 or LC140.
 
+**Time Complexity:** `O(N * S ^ 2)`. `N` is the size of `words`, `S` is the average length of word.
+
+**Space Complexity:** `O(N + S)`.
+
+### Related
 139: Word Break.
 
 140: Word Break II.
--- src/main/java/LeetCode/LC_401_600/LC472/Solution.java
+++ src/main/java/LeetCode/LC_401_600/LC472/Solution.java
@@ -6,14 +6,17 @@ import java.util.Set;
 import java.util.HashSet;
 import java.util.Arrays;
 
+/**
+ * Solution: dp.
+ */
 public class Solution {
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        List<String> ans = new ArrayList();
-        Set<String> dict = new HashSet(Arrays.asList(words)); // convert to Set to speed up lookup.
-        for(String word : words) {
-            if(word.length() == 0) continue;
+        List<String> ans = new ArrayList<>();
+        Set<String> dict = new HashSet<>(Arrays.asList(words)); // convert to Set to speed up lookup.
+        for (String word : words) {
+            if (word.length() == 0) continue;
             dict.remove(word);
-            if(wordBreak(word, dict)) ans.add(word);
+            if (wordBreak(word, dict)) ans.add(word);
             dict.add(word);
         }
         return ans;
@@ -21,11 +24,11 @@ public class Solution {
 
     public boolean wordBreak(String s, Set<String> dict) {
         // dp[i] means the first i characters of s can be split
-        boolean[] dp = new boolean[s.length()+1];
+        boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true; // base case
-        for(int i=1; i<=s.length(); i++) {
-            for(int j=0; j<i; j++) {
-                if(dp[j] && dict.contains(s.substring(j, i))) {
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.contains(s.substring(j, i))) {
                     dp[i] = true;
                     break; // need to break the loop immediately, otherwise causing TLE.
                 }
