bd0f96181bd5e7f29ee45cea1e59ea0408f64f37
Luyi Song <jasongluyi@gmail.com>
2022-10-17 19:43:39 -0400

    LeetCode-10: format

--- src/main/java/LeetCode/LC_1_200/LC10/README.md
+++ src/main/java/LeetCode/LC_1_200/LC10/README.md
@@ -3,18 +3,18 @@
 ### Solution: DP
 `dp[i][j]`: `s` substring with length `i` matches `p` substring with length `j` or not.
 
-Assuming for a given `s` substring with length `i` and `p` substring with length `j`, their corresponding characters at `i-1` and `j-1` are:
+Assuming for a given `s` substring with length `i` and `p` substring with length `j`, their corresponding characters at `i - 1` and `j - 1` are:
 ```
-char ch1 = s.charAt(i-1);
-char ch2 = p.charAt(j-1);
+char ch1 = s.charAt(i - 1);
+char ch2 = p.charAt(j - 1);
 ```
 Several cases need to consider here:
 
-- Case 1: `ch1 == ch2`, then `dp[i][j] = dp[i-1][j-1]`.
-- Case 2: `ch2 == '.'`, then `dp[i][j] = dp[i-1][j-1]`.
-- Case 3: `ch2 == '*'`, here we need to treat `p.charAt(j-2)` plus `*` as a combo, for example: `a*`. Note `p.charAt(j-2)` is character before `*`, i.e. means `a` here.
-    - We can treat `*a` as empty here, so `dp[i][j] = dp[i][j-2]`.
-    - If `p.charAt(j-2) == ch1` OR `p.charAt(j-2) == '.'`, so `dp[i][j] = dp[i-1][j]`, we allow `*a` repeat at least 1 times.
+- Case 1: `ch1 == ch2`, then `dp[i][j] = dp[i - 1][j - 1]`.
+- Case 2: `ch2 == '.'`, then `dp[i][j] = dp[i - 1][j - 1]`.
+- Case 3: `ch2 == '*'`, here we need to treat `p.charAt(j - 2)` plus `*` as a combo, for example: `a*`. Note `p.charAt(j - 2)` is character before `*`, i.e. means `a` here.
+    - We can treat `*a` as empty here, so `dp[i][j] = dp[i][j - 2]`.
+    - If `p.charAt(j - 2) == ch1` OR `p.charAt(j - 2) == '.'`, so `dp[i][j] = dp[i - 1][j]`, we allow `*a` repeat at least 1 times.
 
 **Time Complexity:** `O(M * N)`, `M` is the length of `s`, `N` is the length of `p`. Because we are calculating every `dp[i][j]`.
 
--- src/main/java/LeetCode/LC_1_200/LC10/Solution.java
+++ src/main/java/LeetCode/LC_1_200/LC10/Solution.java
@@ -1,30 +1,33 @@
 package LeetCode.LC_1_200.LC10;
 
+/**
+ * Solution: DP.
+ */
 public class Solution {
     public boolean isMatch(String s, String p) {
-        int len1 = s.length(), len2 = p.length();
-        boolean[][] dp = new boolean[len1+1][len2+1];
+        int n1 = s.length(), n2 = p.length();
+        boolean[][] dp = new boolean[n1 + 1][n2 + 1];
         dp[0][0] = true;
-        // Initialization.
-        for(int i=1; i<len2+1; i++) {
-            if(p.charAt(i-1) == '*' && dp[0][i-2]) { // e.g.: "" matches "a*"
-                dp[0][i] = true;
+        // step 1: initialization.
+        for (int i = 1; i <= n2; i++) {
+            if (p.charAt(i - 1) == '*') { // e.g.: "" matches "a*"
+                dp[0][i] = dp[0][i - 2];
             }
         }
 
-        // DP
-        for (int i=1; i<len1+1; i++) {
-            for (int j=1; j<len2+1; j++) {
-                char ch1 = s.charAt(i-1);
-                char ch2 = p.charAt(j-1);
+        // step 2: DP.
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                char ch1 = s.charAt(i - 1);
+                char ch2 = p.charAt(j - 1);
                 if (ch1 == ch2 || ch2 == '.') { // Case 1 & 2.
-                    dp[i][j] = dp[i-1][j-1];
+                    dp[i][j] = dp[i - 1][j - 1];
                 }
                 if (ch2 == '*') { // Case 3.
-                    dp[i][j] = dp[i][j-2] || ((p.charAt(j-2) == ch1 || p.charAt(j-2) == '.') && dp[i-1][j]);
+                    dp[i][j] = dp[i][j - 2] || ((p.charAt(j - 2) == ch1 || p.charAt(j - 2) == '.') && dp[i - 1][j]);
                 }
             }
         }
-        return dp[len1][len2];
+        return dp[n1][n2];
     }
 }
